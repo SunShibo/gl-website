a0907e40b79ea998625263014fddfeba7eb850f4
dingxinyang <475341496@qq.com>
2018-12-26 17:32:44 +0800

    页面更新

---
+++ src/main/webapp/js/loadcss.js
@@ -0,0 +1,3 @@
+document.write("<link rel='stylesheet' href='css/bootstrap.min.css'>");
+document.write("<link rel='stylesheet' href='css/swiper.min.css'>");
+document.write('<link rel="stylesheet" type="text/css" href="css/style.css">');
---
+++ src/main/webapp/js/loadjs.js
@@ -0,0 +1,3 @@
+document.write("<script src='js/jquery-3.3.1.min.js' type='text/javascript' charset='utf-8'></script>");
+document.write("<script src='js/bootstrap.js' type='text/javascript' charset='utf-8'></script>");
+document.write("<script src='js/swiper.min.js'></script>");
