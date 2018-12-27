8857406138f5c0c45c892db60bd8e4128a78f871
xiaowen1024 <15214440324@163.com>
2018-12-27 15:37:53 +0800

    第二版提交 : 后台管理菜单

--- src/main/java/com/gl/website/web/controller/BackgroundHomeController.java
+++ src/main/java/com/gl/website/web/controller/BackgroundHomeController.java
@@ -1,8 +1,5 @@
 package com.gl.website.web.controller;
-import com.gl.website.entity.bo.AdminBO;
-import com.gl.website.entity.bo.HomepageFirstBO;
-import com.gl.website.entity.bo.HomepageThreeBO;
-import com.gl.website.entity.bo.HomepageTwoBO;
+import com.gl.website.entity.bo.*;
 import com.gl.website.entity.dto.ResultDTOBuilder;
 import com.gl.website.service.HomepageService;
 import com.gl.website.util.JsonUtils;
@@ -13,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -24,7 +22,7 @@ public class BackgroundHomeController   extends BaseCotroller {
     private HomepageService  homepageService;
 
 
-  /*  @RequestMapping("/query")//查询
+   @RequestMapping("/query")//查询
     public void query(HttpServletResponse response, HttpServletRequest request) {
         AdminBO userBO = super.getLoginUser(request) ;
         if (userBO==null){
@@ -33,15 +31,23 @@ public class BackgroundHomeController   extends BaseCotroller {
             return;
         }
 
-        //查询首页头
-        HomepageFirstBO allHomepageFirstMesg = homepageService.getAllHomepageFirstMesg();  //首页模块一
-        HomepageTwoBO allHomepageTwoMesg = homepageService.getAllHomepageTwoMesg();      //首页 模块二
-        List<HomepageThreeBO> allHomepageThreeMesg = homepageService.getAllHomepageThreeMesg(); //首页 模块二
 
-        String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.success(allHomepageFirstMesg));
-        safeTextPrint(response, json);
+       HashMap<String, Object> map = new HashMap<String, Object>();
+       HomepageFirstBO homeModel1 = homepageService.getAllHomepageFirstMesg();
+       HomepageTwoBO homeModel2 = homepageService.getAllHomepageTwoMesg();
+       List<HomepageThreeBO> homeModel3 = homepageService.getAllHomepageThreeMesg();
+       HomepageFourBO homeModel4 = homepageService.getAllHomepageFourMesg();
+       List<HomepageFiveBO> homeModel5 = homepageService.getAllHomepageFiveMesg();
+       map.put("homeModel1", homeModel1);
+       map.put("homeModel2", homeModel2);
+       map.put("homeModel3", homeModel3);
+       map.put("homeModel4", homeModel4);
+       map.put("homeModel5", homeModel5);
+       String json = JsonUtils.getJsonString4JavaPOJO(ResultDTOBuilder.success(map));
+       safeTextPrint(response, json);
+       return;
     }
-*/
+
 
 
 }
