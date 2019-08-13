15594d84758d4b9d6941f518153a72f08a274225
hp <475341496@qq.com>
2019-08-13 16:05:49 +0800

    页面更新

--- src/main/webapp/js/loadcss.js
+++ src/main/webapp/js/loadcss.js
@@ -1,3 +1,4 @@
 document.write("<link rel='stylesheet' href='css/bootstrap.min.css'>");
 document.write("<link rel='stylesheet' href='css/swiper.min.css'>");
 document.write('<link rel="stylesheet" type="text/css" href="css/style.css">');
+document.write('<link rel="shortcut icon" href=image/32px.png type=image/x-icon>');
